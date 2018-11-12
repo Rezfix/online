b4320319d4b21e639e502e454764b8f4de3d8a2d
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-12 21:39:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,51 @@
 const Discord = require("discord.js");
-const HeyIm = new Discord.Client();
+const Reuss = new Discord.Client();
 
-HeyIm.on('ready', () => {
-   HeyIm.user.setGame('Rezfix','https://www.twitch.tv/Rezfix');
+Reuss.on('ready', () => {
+   Reuss.user.setGame('Never Killed','https://www.twitch.tv/Rezfix');
     console.log('---------------');
-    console.log('HeyIm Online')
+    console.log('Russ Online')
     console.log('---------------')
   });
 
-HeyIm.login(process.env.BOT_TOKEN);
-const AbuA = new Discord.Client();
+Reuss.login(process.env.REUSS)
+const D7omz = new Discord.Client();
 
-AbuA.on('ready', () => {
-   AbuA.user.setGame('ابغا iPhone xs 😢','https://www.twitch.tv/Rezfix');
+D7omz.on('ready', () => {
+   D7omz.user.setGame('مَآْ عَشِقْتُ تُرَآْبَ آَرْضٍ مِثْلَ تُرَآْبُ آْلْسُّعُوْدِيَّةُ','https://www.twitch.tv/Rezfix');
     console.log('---------------');
-    console.log('AbuA Online')
+    console.log('D7omz Online')
     console.log('---------------')
   });
 
-AbuA.login(process.env.BOT_TOKE);
+D7omz.login(process.env.D7OMZ)
+const D7omz2 = new Discord.Client();
+
+D7omz2.on('ready', () => {
+   D7omz2.user.setGame('Raped My Life.','https://www.twitch.tv/Rezfix');
+    console.log('---------------');
+    console.log('D7omz2 Online')
+    console.log('---------------')
+  });
+
+D7omz2.login(process.env.D7OMZ2)
+const Gazi = new Discord.Client();
+
+Gazi.on('ready', () => {
+   Gazi.user.setGame('Hacked Account','https://www.twitch.tv/Rezfix');
+    console.log('---------------');
+    console.log('Gazi Online')
+    console.log('---------------')
+  });
+
+Gazi.login(process.env.GAZI)
+const Sfa7 = new Discord.Client();
+
+Sfa7.on('ready', () => {
+   Sfa7.user.setGame('#Rezfix_i love','https://www.twitch.tv/Rezfix');
+    console.log('---------------');
+    console.log('Sfa7 Online')
+    console.log('---------------')
+  });
+
+Sfa7.login(process.env.SFA7);
