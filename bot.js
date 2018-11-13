c532f4f8b280d880e4f7d2fdbd3b060cdf1f17be
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-13 08:23:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,23 +29,3 @@ D7omz2.on('ready', () => {
   });
 
 D7omz2.login(process.env.D7OMZ2)
-const Gazi = new Discord.Client();
-
-Gazi.on('ready', () => {
-   Gazi.user.setGame('Hacked Account','https://www.twitch.tv/Rezfix');
-    console.log('---------------');
-    console.log('Gazi Online')
-    console.log('---------------')
-  });
-
-Gazi.login(process.env.GAZI)
-const Sfa7 = new Discord.Client();
-
-Sfa7.on('ready', () => {
-   Sfa7.user.setGame('#Rezfix_i love','https://www.twitch.tv/Rezfix');
-    console.log('---------------');
-    console.log('Sfa7 Online')
-    console.log('---------------')
-  });
-
-Sfa7.login(process.env.SFA7);
